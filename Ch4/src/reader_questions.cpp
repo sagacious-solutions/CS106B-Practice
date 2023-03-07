11ec07ca0d894fef656ef450cfdabe83c21edb90
Matt Macdonald <mksmacdonald@gmail.com>
2023-03-06 17:05:31 -0800

    Done through question 4 less board formatting

--- Ch4/src/reader_questions.cpp
+++ Ch4/src/reader_questions.cpp
@@ -1,6 +1,8 @@
 #include <console.h>
 #include <ctime>
 #include <fstream>
+#include <grid.h>
+#include <simpio.h>
 #include <vector.h>
 
 using namespace std;
@@ -108,9 +110,131 @@ void question_1_2_3()
         cout << line << endl;
 }
 
+// ************************************************ Question 04 ************************************************
+
+/// The code in Figure 4-2 shows how to check the rows, columns, and diagonals of a
+/// tic-tac-toe board using a single helper function. That function, however, is coded in
+/// such a way that it only works for 3 x 3 boards. As a first step toward creating a
+/// program that can play tic-tac-toe on larger grids, reimplement the CheckForWin and
+/// CheckLine functions so that they work for square grids of any size.
+
+///*
+//* Checks to see whether the specified player identified by mark
+//* ('X' or 'O') has won the game. To reduce the number of special
+//* cases, this implementation uses the helper function CheckLine.
+//*/
+//bool CheckForWin(Grid<char> & board, char mark) {
+//    for (int i = 0; i < 3; i++) {
+//        if (CheckLine(board, mark, i, 0, 0, 1)) return true;
+//        if (CheckLine(board, mark, 0, i, 1, 0)) return true;
+//    }
+//    if (CheckLine(board, mark, 0, 0, 1, 1)) return true;
+//    return CheckLine(board, mark, 2, 0, -1, 1);
+//}
+///*
+//* Checks a line extending across the board in some direction. The
+//* starting coordinates are given by the row and col parameters.
+//* The direction of motion is specified by dRow and dCol, which
+//* show how to adjust the row and col values on each cycle. For
+//* rows, dRow is always 0; for columns, dCol is 0. For diagonals,
+//* these values will be +1 or -1 depending on the direction.
+//*/
+//bool CheckLine(Grid<char> & board, char mark, int row, int col,
+//               int dRow, int dCol) {
+//    for (int i = 0; i < 3; i++) {
+//        if (board[row][col] != mark) return false;
+//        row += dRow;
+//        col += dCol;
+//    }
+//    return true;
+//}
+
+bool CheckLine(Grid<char> &board, char mark, int row, int col, int dRow, int dCol)
+{
+    for (int i = 0; i < board.numRows(); i++) {
+        if (board[row][col] != mark)
+            return false;
+        row += dRow;
+        col += dCol;
+    }
+    return true;
+}
+
+bool CheckForWin(Grid<char> &board, char mark)
+{
+    for (int i = 0; i < board.numRows(); i++) {
+        if (CheckLine(board, mark, i, 0, 0, 1))
+            return true;
+        if (CheckLine(board, mark, 0, i, 1, 0))
+            return true;
+    }
+    if (CheckLine(board, mark, 0, 0, 1, 1))
+        return true;
+    return CheckLine(board, mark, board.numCols() - 1, 0, -1, 1);
+}
+
+void drawBoard(Grid<char> &board)
+{
+    for (int i = 0; i < board.numRows(); i++) {
+        cout << '|';
+        for (int j = 0; j < board.numCols(); j++) {
+            cout << board[i][j] << '|';
+        }
+        cout << endl;
+    }
+}
+
+void question_04()
+{
+    Grid<char> board(3, 3);
+    board.fill(' ');
+    bool winner = false;
+    char player = 'X';
+
+    while (!winner) {
+        bool validPlay = false;
+        int playX, playY;
+
+        while (!validPlay) {
+            // Get input, offset to match grid index
+            playX = getInteger("Input Row :") - 1;
+            playY = getInteger("Input Col :") - 1;
+
+            /// Check input with offset between 0 and grid length
+            if (playX > board.numRows() - 1 || playY > board.numCols() - 1 || playX < 0
+                || playY < 0) {
+                cout << "Value out of range, try again." << endl;
+                continue;
+            }
+
+            if (board.get(playX, playY) != ' ') {
+                cout << "Space already taken, try again." << endl;
+                continue;
+            }
+
+            validPlay = true;
+        }
+        board.set(playX, playY, player);
+        drawBoard(board);
+        if (CheckForWin(board, 'X')) {
+            cout << "X Wins!";
+            break;
+        }
+        if (winner = CheckForWin(board, 'O')) {
+            cout << "Y Wins!";
+            break;
+        };
+
+        if (player == 'X')
+            player = 'O';
+        else
+            player = 'X';
+    }
+}
+
 int main()
 {
     srand(time(0));
-    question_1_2_3();
+    question_04();
     return 0;
 }
