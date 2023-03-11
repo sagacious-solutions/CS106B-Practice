89449c2dc89ae0e55ef42f5850c2fe0db5cd4b66
Matt Macdonald <mksmacdonald@gmail.com>
2023-03-11 15:20:39 -0800

    Done Question 2. Exponent

--- Ch5-Recursion/src/reader_questions.cpp
+++ Ch5-Recursion/src/reader_questions.cpp
@@ -41,11 +41,22 @@ void question_01()
     outputResult(50, heightFifty);
 }
 
+/**
+ * @brief outputResult Outputs an easily readable string with results for cannonBall fn
+ * @param height
+ * @param balls
+ */
 void outputResult(int height, int balls)
 {
     cout << "Height of " << height << " results in " << balls << " balls total." << endl;
 }
 
+/**
+ * @brief cannonBall given a pyramid of cannon balls where each level contains its heigh in balls squared
+ * This function will tell you the total ammount of cannon balls.
+ * @param height of pyramid
+ * @return Total count of balls
+ */
 int cannonBall(int height)
 {
     if (height == 1) {
@@ -55,9 +66,39 @@ int cannonBall(int height)
     }
 }
 
+// ************************************************ Question 01 ************************************************
+///Unlike many programming languages, C++ does not include a predefined operator
+///that raises a number to a power. As a partial remedy for this deficiency, write a
+///recursive implementation of a function
+///int RaiseIntToPower(int n, int k)
+
+int raisePower(int n, int k);
+
+void question_02()
+{
+    cout << "3 Squared should be 9 : " << raisePower(3, 2) << endl;
+    cout << "9 squared should be 81 : " << raisePower(9, 2) << endl;
+    cout << "4^4 should be 256 : " << raisePower(4, 4) << endl;
+}
+
+/**
+ * @brief raisePower Get the answer to n to the kth power
+ * @param n Number
+ * @param k Power
+ * @return Calculated answer
+ */
+int raisePower(int n, int k)
+{
+    if (k == 0) {
+        return 1;
+    } else {
+        return n * raisePower(n, k - 1);
+    }
+}
+
 int main()
 {
     srand(time(0));
-    question_01();
+    question_02();
     return 0;
 }
