d7772b1a617e66baa140ccbba9401330bbb4cde9
Matt Macdonald <mksmacdonald@gmail.com>
2023-03-13 22:42:52 -0700

    Done Question 5. Recursive Comparison counting

--- Ch5-Recursion/src/reader_questions.cpp
+++ Ch5-Recursion/src/reader_questions.cpp
@@ -1,4 +1,7 @@
 #include <console.h>
+#include <iomanip>
+#include <iostream>
+#include <vector.h>
 
 using namespace std;
 
@@ -217,9 +220,102 @@ int FibRe(int n)
     }
 }
 
+// ************************************************ Question 05 ************************************************
+///For each of the two recursive implementations of the function Fib(n) presented in
+///this chapter, write a recursive function (you can call these CountFib1 and
+///CountFib2 for the two algorithms) that counts the number of function calls made
+///during the evaluation of the corresponding Fibonacci calculation. Write a main
+///program that uses these functions to display a table showing the number of calls
+///made by each algorithm for various values of n , as shown in the following sample
+///run:
+
+int countFib1(int n, int &count);
+void outputRow(int n, int fib1, int fib2);
+int AdditiveSequence(int n, int t0, int t1, int &runCount);
+int countFib2(int n, int &runCount);
+
+void question_05()
+{
+    int *fib1Count = new int;
+    *fib1Count = 0;
+    int *fib2Count = new int;
+    *fib2Count = 0;
+
+    cout << "This program compares the performance of two algorithms to compute the Fibonacci "
+            "sequence."
+         << endl
+         << "Number of calls:" << endl
+         << right << setw(3) << "N" << setw(10) << "Fib1" << setw(7) << "Fib2" << endl
+         << right << setw(3) << "--" << setw(10) << "----" << setw(7) << "----" << endl;
+
+    // Runs the fibonachi function twice on each loop with 2 different algorithms.
+    // Counts the recursive calls to the functions and displays it.
+    for (int i = MIN_INDEX; i <= MAX_INDEX; i++) {
+        countFib1(i, *fib1Count);
+        countFib2(i, *fib2Count);
+        outputRow(i, *fib1Count, *fib2Count);
+        *fib1Count = 0;
+        *fib2Count = 0;
+    }
+}
+
+/**
+ * @brief outputRow outputs the data with the correct formatting for each iteration of the question_05 loop
+ * @param n Iterations to run function on
+ * @param fib1 Times first recursive function ran
+ * @param fib2 Times the second resursive function ran
+ */
+void outputRow(int n, int fib1, int fib2)
+{
+    cout << right << setw(3) << n << setw(10) << fib1 << setw(7) << fib2 << endl;
+}
+
+/**
+ * @brief countFib1 Recursive fib function from the reader with counter pointer added
+ * @param n
+ * @param count
+ * @return 
+ */
+int countFib1(int n, int &count)
+{
+    count++;
+    if (n < 2) {
+        return n;
+    } else {
+        return countFib1(n - 1, count) + countFib1(n - 2, count);
+    }
+}
+
+/**
+ * @brief countFib2 Another Recursive fib function from the reader with counter pointer added
+ * @param n
+ * @param count
+ * @return 
+ */
+int countFib2(int n, int &runCount)
+{
+    return AdditiveSequence(n, 0, 1, runCount);
+}
+
+/**
+ * @brief AdditiveSequence Another Recursive fib function from the reader with counter pointer added
+ * @param n
+ * @param count
+ * @return 
+ */
+int AdditiveSequence(int n, int t0, int t1, int &runCount)
+{
+    runCount++;
+    if (n == 0)
+        return t0;
+    if (n == 1)
+        return t1;
+    return AdditiveSequence(n - 1, t1, t0 + t1, runCount);
+}
+
 int main()
 {
     srand(time(0));
-    question_04();
+    question_05();
     return 0;
 }
